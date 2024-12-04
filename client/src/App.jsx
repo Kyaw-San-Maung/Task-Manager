c59b350b49e90ed30315934c34fe04ecd75405da
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-12-04 11:25:01 +0630

    feat: implement the Login Form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,35 @@
 import "./App.css";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { Dashboard } from "./pages/Dashboard";
-import { Layout } from "./pages/Layout";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 import Tasks from "./pages/Tasks";
 import Team from "./pages/Teasm";
 import Trash from "./pages/Trash";
 import TaskDetails from "./pages/TaskDetails";
 import Login from "./pages/Login";
 import { Toaster } from "sonner";
+
+function Layout() {
+  const user = "";
+
+  const location = useLocation();
+
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* <Sidebar /> */}
+      </div>
+      {/* <MobileSidebar /> */}
+      <div className="flex-1 overflow-y-auto">
+        {/* <Navbar /> */}
+        <div className="p-4 2xl:px-10">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/log-in" state={{ Form: location }} replace />
+  );
+}
 
 function App() {
   return (
