f55b4a10a998ea3ff7c5bc3689bbe88a4eb75d8c
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-12-17 00:42:11 +0630

    fix: design reponsive problem

--- client/src/componets/Button.jsx
+++ client/src/componets/Button.jsx
@@ -1,5 +1,20 @@
+import clsx from "clsx";
 import React from "react";
 
-export const Button = () => {
-  return <div>Button</div>;
+export const Button = ({
+  className,
+  icon,
+  label,
+  type,
+  onClick = () => {},
+}) => {
+  return (
+    <div>
+      <button
+        type={type || "button"}
+        className={clsx("px-3 py-2 outline-none", className)}>
+        <span>{label}</span>
+      </button>
+    </div>
+  );
 };
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import TextBox from "../componets/TextBox";
+import { Button } from "../componets/Button";
 
 const Login = () => {
   const user = "";
@@ -23,14 +24,14 @@ const Login = () => {
 
   return (
     <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
-      <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
+      <div className="w-full md:w-auto gap-0 flex md:gap-56 flex-col md:flex-row items-center justify-center">
         {/* Left side */}
         <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
           <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
             <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600">
               Manage all your task in one place1
             </span>
-            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
+            <p className="flex flex-col gap-0 text-4xl md:text-6xl font-black text-blue-700 leading-tight">
               <span>Cloud-Based</span>
               <span>Task Manager</span>
             </p>
@@ -39,6 +40,7 @@ const Login = () => {
             </div>
           </div>
         </div>
+
         {/* Right side */}
         <div className="w-full md:w-1 p-4 md:p-1 flex flex-col justify-center items-center">
           <form
@@ -81,6 +83,12 @@ const Login = () => {
               <span className="text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer">
                 Forget Password?
               </span>
+
+              <Button
+                type="submit"
+                label="submit"
+                className="w-full bg-blue-600 rounded-full"
+              />
             </div>
           </form>
         </div>
