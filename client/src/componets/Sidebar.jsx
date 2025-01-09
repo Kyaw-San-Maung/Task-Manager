6f341e4fc1ff65403c6b06689a9d9c5f94cfe7b2
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2025-01-09 10:31:52 +0630

    feat: updating the sidebar

--- client/src/componets/Sidebar.jsx
+++ client/src/componets/Sidebar.jsx
@@ -1,5 +1,74 @@
+import {
+  MdDashboard,
+  MdOutlineAddTask,
+  MdOutlinePendingActions,
+  MdTaskAlt,
+} from "react-icons/md";
+import { FaTasks, FaTrashAlt, FaUsers } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { authActions } from "../redux/slices/authSlice";
+const linkData = [
+  {
+    label: "Dashboard",
+    link: "dashboard",
+    icon: <MdDashboard />,
+  },
+  {
+    label: "Tasks",
+    link: "tasks",
+    icon: <FaTasks />,
+  },
+  {
+    label: "Completed",
+    link: "completed/completed",
+    icon: <MdTaskAlt />,
+  },
+  {
+    label: "In Progress",
+    link: "in-progress/in progress",
+    icon: <MdOutlinePendingActions />,
+  },
+  {
+    label: "To Do",
+    link: "todo/todo",
+    icon: <MdOutlinePendingActions />,
+  },
+  {
+    label: "Team",
+    link: "team",
+    icon: <FaUsers />,
+  },
+  {
+    label: "Trash",
+    link: "trashed",
+    icon: <FaTrashAlt />,
+  },
+];
+
 function Sidebar() {
-  return <div>Ok</div>;
+  const user = true;
+
+  const dispatch = useDispatch();
+
+  const Location = useLocation();
+
+  const sidebarLinks = user?.isAdmin ? linkData : linkData.slice(0, 5);
+
+  const closeSidebar = () => {
+    dispatch(authActions.setOpenSlider(false));
+  };
+
+  return (
+    <div className="w-full h-full flex flex-col gap-6 p-5">
+      <h1 className="flex gap-1 items-center">
+        <p className="bg-blue-600 p-2 rounded-full">
+          <MdOutlineAddTask className="text-white text-2xl font-black" />
+        </p>
+        <span className="text-2xl font-bold text-black">Task Me</span>
+      </h1>
+    </div>
+  );
 }
 
 export default Sidebar;
